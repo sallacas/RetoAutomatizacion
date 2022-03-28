0596b0d922c8d533a70177d96290a2825bb66110
Johnatan Casallas <jdcasallas12@gmail.com>
2022-03-27 19:39:23 -0500

    Serenify conf update

--- src/test/java/starter/ui/dashboard/DashboardOverview.java
+++ src/test/java/starter/ui/dashboard/DashboardOverview.java
@@ -6,7 +6,7 @@ public class DashboardOverview {
     //Matcher
     public static By DASHBOARD_TITLE = By.xpath("//*[@id=\"s-DashboardPage\"]/main/section/section/h1");
     //Business section
-    public static By ORG_OPTION = By.xpath("/html/body/aside/div[2]/div[2]/div[1]/ul/li[3]/ul/li[1]/a");
+    public static By ORG_OPTION = By.xpath("//a[@href='#nav_menu1_3_1']");
     public static By BUSINESS_UNIT = By.xpath("//*[@id=\"nav_menu1_3_1\"]/li[1]/a");
     //Meeting section
     public static By MET_OPTION = By.xpath("/html/body/aside/div[2]/div[2]/div[1]/ul/li[3]/ul/li[2]/a");
